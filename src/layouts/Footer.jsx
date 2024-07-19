d8a09ad5a88337d4193d1b6ab4a553b6da7c2de6
sayanmarko <sayan@markobrando.com>
2024-07-19 17:10:19 +0530

    create header footer

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,7 +1,97 @@
+import Image from "next/image";
+import logo from "../../public/logo.png";
+import Link from "next/link";
+
+//images & icons
+import envlop from "../../public/icons/envlop.png";
+import phone from "../../public/icons/phone.png";
+import mapPin from "../../public/icons/map-pin.png";
+
 export default function Footer() {
   return (
-    <>
-      <div className="container-fluid">footer</div>
-    </>
+    <footer className="container">
+      <div className="row">
+        <div className="col-md-4">
+          <Image src={logo} className=" img-fluid logo" />
+          <p className="mt-3 mb-4 pe-md-4">
+            Sinotrade Global Ltd is a company registered in Hong Kong and has
+            successfully been a sourcing agent and indenting agent for the last
+            13 years.
+          </p>
+          {/* <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul> */}
+        </div>
+        <div className="col-6 col-md-2">
+          <h3>Quick Links</h3>
+          <ul>
+            <li>
+              <Link href="/">Product</Link>
+            </li>
+            <li>
+              <Link href="/">Pricing</Link>
+            </li>
+            <li>
+              <Link href="/">Help & support</Link>
+            </li>
+            <li>
+              <Link href="/">FAQ</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="col-6 col-md-2">
+          <h3>Product</h3>
+          <ul>
+            <li>
+              <Link href="/">Chair</Link>
+            </li>
+            <li>
+              <Link href="/">Sofa</Link>
+            </li>
+            <li>
+              <Link href="/">Table</Link>
+            </li>
+            <li>
+              <Link href="/">Lamp</Link>
+            </li>
+            <li>
+              <Link href="/">Mirrors</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="col-md-4">
+          <h3>Contact</h3>
+          <ul className="cta">
+            <li>
+              <Image src={mapPin} />
+              <div className="">
+                <h5 className="">Sinotrade Global Ltd</h5>
+                <p className="">
+                  Sinotrade Global Ltd HKSuite 1009, Dongshan Plaza, No.69,
+                  Xianlie, Road Central, Guangzhou City, Guangdong Province,
+                  China 510095
+                </p>
+              </div>
+            </li>
+            <li>
+              <Image src={phone} />
+              <div className="">
+                <a href="tel:000">+ 86 20 87327751</a>
+                <br />
+                <a href="tel:000">+ 86 20 87326815</a>
+              </div>
+            </li>
+            <li>
+              <Image src={envlop} />
+              <a href="mailto:info@sinotradeglobal.com">
+                info@sinotradeglobal.com
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
   );
 }
--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -2,28 +2,108 @@ import next from "next";
 import Image from "next/image";
 import Link from "next/link";
 
+// import Nav from "react-bootstrap/Nav";
+// import Dropdown from "react-bootstrap/Dropdown";
+import Navbar from "react-bootstrap/Navbar";
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Offcanvas from "react-bootstrap/Offcanvas";
+
+//
+import logo from "../../public/logo.png";
+
 export default function Header() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <header class="">
-      <nav class="navbar navbar-expand-lg">
-        <div class="container-fluid">
-          <Link class="navbar-brand" href="#">
+      <nav class="navbar navbar-expand-lg p-0">
+        <div class="container pt-3">
+          <Navbar.Brand href="/">
             <div class="logo">
-              <Image src="../../public/logo.png" alt="" width="auto" />
+              <Image src={logo} alt="" className="img-fluid" />
             </div>
-          </Link>
-
-          <button
-            class="btn d-lg-none"
-            type="button"
-            data-bs-toggle="offcanvas"
-            data-bs-target="#offcanvasRight"
-            aria-controls="offcanvasRight"
-          >
+          </Navbar.Brand>
+          <Button variant="" onClick={handleShow} className="d-block d-lg-none">
             <span class="navbar-toggler-icon"></span>
-          </button>
+          </Button>
 
-          <div
+          <Offcanvas show={show} onHide={handleClose} placement={"end"}>
+            <Offcanvas.Header closeButton></Offcanvas.Header>
+            <Offcanvas.Body>
+              <ul class="navbar-nav align-items-center">
+                <li class="nav-item">
+                  <Link class="nav-link active" href="./">
+                    About Us
+                  </Link>
+                </li>
+                <li class="nav-item">
+                  <Link class="nav-link" href="./">
+                    Project
+                  </Link>
+                </li>
+                <li class="nav-item">
+                  <Link class="nav-link" href="./">
+                    Our Products
+                  </Link>
+                </li>
+                <li class="nav-item">
+                  <Link class="nav-link" href="./">
+                    Our Services
+                  </Link>
+                </li>
+                <li class="nav-item">
+                  <Link class="nav-link" href="./">
+                    Catalogues
+                  </Link>
+                </li>
+                <li class="nav-item">
+                  <Link class="cta-btn" href="./">
+                    Contact Us
+                  </Link>
+                </li>
+              </ul>
+            </Offcanvas.Body>
+          </Offcanvas>
+
+          {/*  */}
+
+          <ul class="navbar-nav align-items-center d-none d-lg-flex">
+            <li class="nav-item">
+              <Link class="nav-link active" href="./">
+                About Us
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" href="./">
+                Project
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" href="./">
+                Our Products
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" href="./">
+                Our Services
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" href="./">
+                Catalogues
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link class="cta-btn" href="./">
+                Contact Us
+              </Link>
+            </li>
+          </ul>
+
+          {/* <div
             class="offcanvas offcanvas-end"
             tabindex="-1"
             id="offcanvasRight"
@@ -37,35 +117,9 @@ export default function Header() {
               ></button>
             </div>
             <div class="offcanvas-body justify-content-lg-end">
-              <ul class="navbar-nav align-items-center">
-                <li class="nav-item">
-                  <Link class="nav-link active" href="./">
-                    Home
-                  </Link>
-                </li>
-                <li class="nav-item">
-                  <Link class="nav-link" href="./">
-                    About Us
-                  </Link>
-                </li>
-                <li class="nav-item">
-                  <Link class="nav-link" href="./">
-                    Our Services
-                  </Link>
-                </li>
-                <li class="nav-item">
-                  <Link class="nav-link" href="./">
-                    Our Client Reviews
-                  </Link>
-                </li>
-                <li class="nav-item">
-                  <Link class="nav-link" href="./">
-                    Contact Us
-                  </Link>
-                </li>
-              </ul>
+              
             </div>
-          </div>
+          </div> */}
         </div>
       </nav>
     </header>
